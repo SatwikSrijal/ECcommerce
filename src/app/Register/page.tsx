5f793decb9570a5d056f9a42cb884700cbf0c7a5
SatwikSrijal <111478389+SatwikSrijal@users.noreply.github.com>
2025-04-18 22:30:02 +0530

    Delete src/app directory

--- src/app/Register/page.tsx
+++
@@ -1,9 +0,0 @@
-// import type {Metatdata} from "next";
-// import RegisterPage from "./Register";
-// export const metadata: Metadata = {
-//     title: "ECOMM | Register",
-//     description: "ecomm.com: Register & Create Account",
-// };
-// export default function About(){
-//     return <RegisterPage/>
-// }
